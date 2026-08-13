11cfef33b5acf656a4a88de7f00c5108c35e3ecf
silvajuan09 <juandavi2406@gmail.com>
2026-08-13 13:58:46 +0000

    Lista 8 - Início

---
+++ Lista_8_Streamlit/paciente.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+class Paciente:
+    def __init__(self, nome, cpf, telefone, nascimento):
+        self.set_nome(nome)
+        self.set_cpf(cpf)
+        self.set_telefone(telefone)
+        self.set_nascimento(nascimento)
+    def set_nome(self, nome):
+        if nome == "": raise ValueError('Nome deve ser preenchido')
+        self.__nome = nome
+    def set_cpf(self, cpf):
+        if len(cpf) != 11: raise ValueError('Preencha os 11 dígitos de CPF (apenas algarismos)')
+        self.__cpf = cpf
+    def set_fone(self, fone):
+        if fone == "": raise ValueError('O telefone deve ser informado')
+        self.__fone = fone
+    def set_nascimento(self, nascimento):
+        if nascimento > datetime.now(): raise ValueError('A data não pode estar no futuro')
+        self.__nascimento = nascimento
+    def get_nome(self): return self.__nome 
+    def get_cpf(self): return self.__cpf
+    def get_fone(self): return self.__fone
+    def get_nascimento(self): return self.__nascimento
+    def idade(self):
+        
